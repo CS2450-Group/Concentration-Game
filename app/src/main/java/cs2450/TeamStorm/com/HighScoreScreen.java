1df69a3039df3ebd734936aab5e7b1fe580cf527
Owen Lovett <olovett214@gmail.com>
2022-12-01 17:53:00 -0800

    added High Scores Layout

--- app/src/main/java/cs2450/TeamStorm/com/HighScoreScreen.java
+++ app/src/main/java/cs2450/TeamStorm/com/HighScoreScreen.java
@@ -3,12 +3,28 @@ package cs2450.TeamStorm.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class HighScoreScreen extends AppCompatActivity {
+
+    TextView highScore1;
+    TextView highScore2;
+    TextView highScore3;
+    String[][] scores = new String[9][6];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score_screen);
+
+        highScore1 = (TextView) findViewById(R.id.highScore1);
+        highScore2 = (TextView) findViewById(R.id.highScore2);
+        highScore3 = (TextView) findViewById(R.id.highScore3);
     }
+
 }
